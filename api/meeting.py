777738eb32b24ce3ce3cc40d371839fe1984d8b4
gilnyangyi-test <gilnyangyi@gmail.com>
2026-09-23 13:07:38 +0900

    Show meeting query in initial Dooray response

--- api/meeting.py
+++ api/meeting.py
@@ -196,5 +196,5 @@ async def meeting_command(req: Request):
 
     return pack({
         "responseType": "inChannel",
-        "text": f"🔎 회의실 정보를 조회 중입니다. 완료되면 이 메시지를 결과로 바꾸겠습니다. (작업 #{issue_number})",
+        "text": f'회의실 정보를 조회 중입니다.\n질의 : "{query}"',
     })
